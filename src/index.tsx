35b5ef20aaca7272ecb567fb1eebee2579d14c68
jumpstonik <xtineen@gmail.com>
2022-05-07 03:08:23 -0600

    Updates

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import HeroesPage from "./components/HeroesPage";
-
-const divRoot = document.querySelector("#root");
-// insertar aqui el componente principal
-ReactDOM.render(<HeroesPage />, divRoot);
